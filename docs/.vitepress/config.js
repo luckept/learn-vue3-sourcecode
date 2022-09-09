975563c37a24dfe0b900648adc013f3be5f7b15d
luckept <luckept@qq.com>
2022-09-09 16:44:35 +0800

    chore: vitepress support

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitepress'
+
+const themeConfig = {
+  logo: '/logo.png',
+  socialLinks: [
+    { icon: 'github', link: 'https://github.com/luckept/learn-vue3-sourcecode' },
+  ],
+  footer: {
+    message: 'Released under the MIT License.',
+    copyright: 'Copyright © 2022-present Luckept'
+  },
+}
+
+export default defineConfig({
+  title: 'Learn Vue3 Sourcecode',
+  themeConfig,
+  appearance: false,
+  base: '/learn-vue3-sourcecode/',
+})
+
